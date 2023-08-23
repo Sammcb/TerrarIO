4b919aad553b6f52faaad5749e67bf05faf5e727
Samuel McBroom <sam@sammcb.com>
2023-08-23 11:34:05 -0700

    Initial Commit

---
+++ TerrarIO/TerrarIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerrarIOApp.swift
+//  TerrarIO
+//
+//  Created by Sam McBroom on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TerrarIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
